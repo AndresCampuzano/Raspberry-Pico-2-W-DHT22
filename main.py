11e550c1bf8297593ea4cfb7d5e378f93771cfc8
Andres Campuzano <campuxano88@gmail.com>
2025-04-06 21:06:15 -0500

    Add WiFi connection and data posting functionality to main.py

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from machine import Pin
 from dht import DHT22
 from time import sleep
+import urequests
+import network  # Import network module
 
-sensor = DHT22(Pin(2)) # GPIO 2
-led = Pin("LED", Pin.OUT) 
-led.on() 
+sensor = DHT22(Pin(2))
+raspberryLed = Pin("LED", Pin.OUT) 
+raspberryLed.on()
+customLed = Pin(15, Pin.OUT)
+customLed.on()
 
-# Conections guide:
+# Connect to WiFi
+ssid = '-'
+password = '-'
+
+def connect_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    timeout = 10  # Timeout after 10 seconds
+    while not wlan.isconnected() and timeout > 0:
+        print('Connecting to network...')
+        sleep(1)
+        timeout -= 1
+    if wlan.isconnected():
+        print('Connected to', ssid)
+        print('Network config:', wlan.ifconfig())
+    else:
+        print('Failed to connect to network')
+        wlan.active(False)
+
+connect_wifi()  # Connect to WiFi
+
+# Connections guide:
 # Power: Pin 36 (3V3 (OUT))
 # Data: Pin 4 (GP2)
 # Ground: Pin 8 (GND)
@@ -14,12 +40,31 @@ led.on()
 def read_sensor():
     try:
         sensor.measure()
-        temperature = sensor.temperature()
+        temperature = sensor.temperature() - 2  # Adjust DTH22 sensor reading due to calibration
         humidity = sensor.humidity()
         return temperature, humidity
     except Exception as e:
         print('Failed to read sensor:', str(e))
         return None, None
+
+def post_weather_data(temp, hum):
+    url = '-'
+    data = {
+        'temperature': temp,
+        'humidity': hum,
+        'city_id': '22fd2af8-2ea4-40f5-96f5-ffc8ab197d9f'
+    }
+    try:
+        response = urequests.post(url, json=data)
+        if response.status_code == 200:
+            print('Data posted successfully')
+        else:
+            print('Failed to post data:', response.status_code)
+        response.close()
+    except OSError as e:
+        print('Network error posting data:', str(e))
+    except Exception as e:
+        print('Error posting data:', str(e))
 
 print('DHT22 Temperature and Humidity Monitor')
 print('Press Ctrl+C to stop')
@@ -31,9 +76,10 @@ while True:
             print(f'Temperature: {temp:.1f} C')
             print(f'Humidity: {hum:.1f}%')
             print('-------------------')
-        sleep(0.2)  
+            post_weather_data(temp, hum)
+        sleep(5)  
     except KeyboardInterrupt:
-        led.off()  
+        raspberryLed.off()  
         print('Monitoring stopped')
         break
     except Exception as e:
